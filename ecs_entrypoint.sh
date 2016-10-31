c66e2136de56dafd5f9585a6d16939997c6e4a0b
David Hallum <david@kenzielane.com>
2016-10-31 09:08:58 -0400

    Exposing RETRY_ATTEMPTS and RETRY_INTERNVAL parameters [#133265271]

--- ecs_entrypoint.sh
+++ ecs_entrypoint.sh
@@ -3,7 +3,12 @@
 IP=$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4 | tr -d '\n')
 echo "IP=${IP}"
 
-/bin/registrator            \
-  -ip ${IP}                 \
-  -resync ${RESYNC_SECONDS} \
+RETRY_ATTEMPTS=${RETRY_ATTEMPTS:-10}
+RETRY_INTERVAL=${RETRY_INTERVAL:-500}
+
+/bin/registrator                    \
+  -ip ${IP}                         \
+  -resync ${RESYNC_SECONDS}         \
+  -retry-attempts ${RETRY_ATTEMPTS} \
+  -retry-interval ${RETRY_INTERVAL} \
   consul://${IP}:8500
